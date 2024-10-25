c72b18cbbd460d7adfeb827f20d5ddf504158545
BGNC <bugra34055@hotmail.com>
2024-10-26 01:13:29 +0300

    Initial commit

---
+++ src/test/java/com/bgnc/personalfinancemanagement/PersonalFinanceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgnc.personalfinancemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
